8179569070c61d565abcf61f17b4277322b84342
liuzheng <liuzheng712@gmail.com>
2015-09-22 21:11:00 +0800

    update

--- static/js/app.js
+++ static/js/app.js
@@ -2,15 +2,21 @@
  * Created by liuzheng on 9/22/15.
  */
 'use strict';
-var app = angular.module('NgApp', []);
-app.config(function($interpolateProvider){
-  $interpolateProvider.startSymble('{[{');
-  $interpolateProvider.endSymble('}]}');
+'use strict';
+var NgApp = angular.module('NgApp', ['ngRoute', 'ngCookies']);
+NgApp.config(function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[{');
+    $interpolateProvider.endSymbol('}]}');
 });
-app.controller('SudokuCtrl', function ($scope, $http) {
-  $scope.Matrix = list(81)
+NgApp.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+}]);
+NgApp.controller('SudokuCtrl', function ($scope, $http) {
+  $scope.Matrix = Array(81)
 
-})
+});
 // routeProvider
 //function($routeProvider, $rootScope){
 //  $routeProvider. 
